cf8ac0cbe2512bf09cbf34e9284cc14bda41b75b
Łukasz Chojnowski <lukasz.chojnowski@unitedideas.pl>
2022-01-24 20:46:20 +0000

    Update install.rb

--- install.rb
+++ install.rb
@@ -9,11 +9,14 @@ require 'erb'
 
 # ------------------------------ INSTALL
 
-puts "Going to install bundler..."
-system "gem install bundler"
-puts "Going to install gems..."
-system "bundle install"
-system "cp config/config.example.yml config/config.yml" # TODO: Dostosuj do potrzeb
+# puts "Going to install bundler..."
+# system "gem install bundler"
+# puts "Going to install gems..."
+# system "bundle install"
+# system "cp config/config.example.yml config/config.yml" # TODO: Dostosuj do potrzeb
+
+puts `which ruby`
+puts `which bundle`
 
 # ------------------------------ PRINT SUMMARY
 
